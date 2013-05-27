a0b50c5967b78cef178b1dbc5bd879d2e20ca8f2
Zach Jones <prozachj@gmail.com>
2013-05-27 16:14:07 -0500

    SOA and A for mattjay.com

--- inbound.js
+++ inbound.js
@@ -1,1 +1,18 @@
 var dnsd = require('dnsd');
+
+var server = dnsd.createServer(handler);
+server.zone('mattjay.com', 'ns1.bluehost.com', 'dnsadmin@box713.bluehost.com', 'now', '2h', '30m', '2w', '10m')
+.listen(53, '127.0.0.1');
+console.log('Server running at 127.0.0.1:53');
+
+function handler (req, res) {
+  console.log('%s:%s/%s %j', req.connection.remoteAddress, req.connection.remotePort, req.connection.type, req);
+ 
+  var question = res.question[0], 
+      hostname = question.name; 
+
+  if (question.type == 'A') {
+    res.answer.push({name:hostname, type:'A', data:"66.147.244.213", 'ttl':'14400'});
+  }
+  res.end();
+}
