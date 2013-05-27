fe8e041c33ac5d4c795ada12f9b4fac41a948166
mattjay <matt@mattjay.com>
2013-05-27 22:55:06 +0000

    inbound edits and package.json edit

--- inbound.js
+++ inbound.js
@@ -1,7 +1,7 @@
 var dnsd = require('dnsd');
 
 var server = dnsd.createServer(handler);
-server.zone('mattjay.com', 'ns1.bluehost.com', 'dnsadmin@box713.bluehost.com', 'now', '2h', '30m', '2w', '10m')
+server.zone('mattjay.com', 'ec2-50-17-47-157.compute-1.amazonaws.com', 'matt@mattjay.com', 'now', '2h', '30m', '2w', '10m')
 .listen(53, '127.0.0.1');
 console.log('Server running at 127.0.0.1:53');
 
@@ -12,7 +12,7 @@ function handler (req, res) {
       hostname = question.name; 
 
   if (question.type == 'A') {
-    res.answer.push({name:hostname, type:'A', data:"66.147.244.213"});
+    res.answer.push({name:hostname, type:'A', data:"ProxyLB-1-1731372181.us-east-1.elb.amazonaws.com"});
   }
   res.end();
 }
