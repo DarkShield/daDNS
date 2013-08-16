3745fdec4c6a8786f9aedee617fa860763377f9e
mattjay <matt@mattjay.com>
2013-08-16 21:01:47 +0000

    added dashboard.vicetek.com to outbound DNS

--- outbound.js
+++ outbound.js
@@ -6,14 +6,26 @@ server.zone('mattjay.com', 'ns1.vicetek.com', 'matt@mattjay.com', 'now', '2h', '
 console.log('Server running at 127.0.0.1:53');
 
 function handler (req, res) {
-	  console.log('%s:%s/%s %j', req.connection.remoteAddress, req.connection.remotePort, req.connection.type, req);
+  console.log('%s:%s/%s %j', req.connection.remoteAddress, req.connection.remotePort, req.connection.type, req);
 
-	    var question = res.question[0],
-	          hostname = question.name;
+  var question = res.question[0];
 
-		    if (question.type == 'A') {
-			        res.answer.push({name:hostname, type:'A', data:"66.147.244.213"});
-				  }
-				    res.end();
+  if (question) {
+    var hostname = question.name;
+    if (hostname = 'mattjay.com' || 'www.mattjay.com') {
+      if (question.type == 'A') {
+        res.answer.push({name:hostname, type:'A', data:"66.147.244.213"});
+      }
+    }
+    
+    if (hostname = 'dashboard.vicetek.com') {
+      res.answer.push({name:hostname, type: 'A', data: '54.221.226.142'});
+    }
+
+    res.end();
+  }
+  else {
+    console.log('question undefined');
+  }
 }
 
