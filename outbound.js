1b44c89babbfe67813b00b6d28973600803a73b8
mattjay <matt@mattjay.com>
2014-11-27 03:48:19 +0000

    many outbound.js changes

--- outbound.js
+++ outbound.js
@@ -12,14 +12,73 @@ function handler (req, res) {
 
   if (question) {
     var hostname = question.name;
-    if (hostname = 'mattjay.com' || 'www.mattjay.com') {
+    if (hostname == 'mattjay.com' || hostname == 'www.mattjay.com') {
       if (question.type == 'A') {
         res.answer.push({name:hostname, type:'A', data:"66.147.244.213"});
       }
     }
+
+    if (hostname == 'strider.vicetek.com') {
+      res.answer.push({name:hostname, type: 'A', data: '10.241.81.61'});
+    }
     
-    if (hostname = 'dashboard.vicetek.com') {
+    if (hostname == 'dashboard.vicetek.com') {
       res.answer.push({name:hostname, type: 'A', data: '54.221.226.142'});
+    }
+
+    if (hostname == 'urbanhydro.org' || hostname == 'www.urbanhydro.org') {
+      res.answer.push({name:hostname, type: 'A', data: '184.168.136.128'});
+    }
+
+    if (hostname == 'supercroppers.com' || hostname == 'www.supercroppers.com') {
+      res.answer.push({name:hostname, type: 'A', data: '192.254.233.219'});
+    }
+
+    if (hostname == 'darkshield.io' || hostname == 'www.darkshield.io') {
+      res.answer.push({name: hostname, type: 'A', data: '54.221.245.88'});
+    }
+
+    if (hostname == 'dashboard.darkshield.io') {
+      res.answer.push({name: hostname, type: 'A', data: '54.221.226.142'})
+    }
+
+    if (hostname == 'edbellis.com' || hostname == 'www.edbellis.com') {
+      res.answer.push({name: hostname, type: 'A', data: '192.0.80.250'})
+    }
+
+    if (hostname == 'blog.risk.io' || hostname == 'blog.riskio.com') {
+      res.answer.push({name: hostname, type: 'A', data: '54.148.2.201'});
+    }
+
+    if (hostname == 'collector.newrelic.com') {
+      res.answer.push({name: hostname, type: 'A', data: '50.31.164.140'});
+    }
+    if (hostname == 'collector-1.newrelic.com') {
+      res.answer.push({name: hostname, type: 'A', data: '50.31.164.145'});
+    }
+    if (hostname == 'collector-2.newrelic.com') {
+      res.answer.push({name: hostname, type: 'A', data: '50.31.164.146'});
+    }
+    if (hostname == 'collector-3.newrelic.com') {
+      res.answer.push({name: hostname, type: 'A', data: '50.31.164.147'});
+    }
+    if (hostname == 'collector-4.newrelic.com') {
+      res.answer.push({name: hostname, type: 'A', data: '50.31.164.148'});
+    }
+    if (hostname == 'collector-5.newrelic.com') {
+      res.answer.push({name: hostname, type: 'A', data: '50.31.164.149'});
+    }
+    if (hostname == 'collector-6.newrelic.com') {
+      res.answer.push({name: hostname, type: 'A', data: '50.31.164.150'});
+    }
+    if (hostname == 'collector-7.newrelic.com') {
+      res.answer.push({name: hostname, type: 'A', data: '50.31.164.151'});
+    }
+    if (hostname == 'collector-8.newrelic.com') {
+      res.answer.push({name: hostname, type: 'A', data: '50.31.164.152'});
+    }
+    if (hostname == 'collector-9.newrelic.com') {
+      res.answer.push({name: hostname, type: 'A', data: '50.31.164.153'});
     }
 
     res.end();
